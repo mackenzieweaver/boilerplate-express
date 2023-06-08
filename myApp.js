934f68adc6ae84c8a7582eb46973b39e01958a6b
Mackenzie Weaver <macknz7@gmail.com>
2023-06-08 15:26:44 -0400

    all Basic Node and Express challenges

--- myApp.js
+++ myApp.js
@@ -1,39 +1,45 @@
-let express = require('express');
-let app = express();
+let express = require('express')
+let app = express()
+require('dotenv').config()
+const bodyParser = require('body-parser')
 
+app.use('/public', express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url} - ${req.ip}`)
+    next()
+})
 
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/views/index.html`)
+})
 
+app.get('/json', (req, res) => {
+    let message = 'Hello Json'
+    if(process.env.MESSAGE_STYLE === 'uppercase'){
+        message = message.toUpperCase()
+    }
+    res.json({message})
+})
 
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString()
+    next()
+}, (req, res) => {
+    res.json({ time: req.time })
+})
 
+app.get('/:word/echo', (req, res) => {
+    res.json({ echo: req.params.word })
+})
 
+app.route('/name')
+    .get((req, res) => {
+        res.json({ name: `${req.query.first} ${req.query.last}` })
+    })
+    .post((req, res) => {
+        res.json({ name: `${req.body.first} ${req.body.last}` })
+    })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app
